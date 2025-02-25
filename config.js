7cd9c383c4e58b77011ff337701aaedbfde389a0
deylinqff <maycraftminecraft4@gmail.com>
2025-02-24 20:15:42 -0600

    Actualizar

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  db: {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'webapp',
+  },
+  payment: {
+    stripeSecretKey: 'YOUR_STRIPE_SECRET_KEY',
+    paypalClientId: 'YOUR_PAYPAL_CLIENT_ID',
+    paypalSecret: 'YOUR_PAYPAL_SECRET',
+  },
+};
