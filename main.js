deb907b0a800105dd677cf886e0e0f620da2575d
deylinqff <maycraftminecraft4@gmail.com>
2025-02-25 12:39:28 -0600

    Actualizar main.js

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-document.getElementById("form-registro").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("registro").style.display = "none";
-    document.getElementById("seleccion-bots").style.display = "block";
-});
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("form-registro").addEventListener("submit", function(event) {
+        event.preventDefault();
+        document.getElementById("registro").style.display = "none";
+        document.getElementById("seleccion-bots").style.display = "block";
+    });
 
-function mostrarPago() {
-    document.getElementById("seleccion-bots").style.display = "none";
-    document.getElementById("pago").style.display = "block";
-}
+    window.mostrarPago = function() {
+        document.getElementById("seleccion-bots").style.display = "none";
+        document.getElementById("pago").style.display = "block";
+    };
 
-document.getElementById("form-pago").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Pago realizado con éxito. ¡Gracias por tu compra!");
+    document.getElementById("form-pago").addEventListener("submit", function(event) {
+        event.preventDefault();
+        alert("Pago realizado con éxito. ¡Gracias por tu compra!");
+    });
 });
