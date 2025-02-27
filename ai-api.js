1df7459997c4b824fa2cdea3cef203e6178ddc12
deylinqff <maycraftminecraft4@gmail.com>
2025-02-27 10:43:41 -0600

    Actualizar ai-api.js

--- ai-api.js
+++ ai-api.js
@@ -2,10 +2,10 @@ const API_URL = "https://api.ryzendesu.vip/api/ai/gemini-pro?text=";
 
 async function obtenerRespuestaIA(mensaje) {
     try {
-        const promptBase = `Tu nombre es Kirito-Bot, fuiste creado por Deylin y solo hablas español. Te gusta ser amigable, divertido y ayudar a los demás. Solo mencionarás tu nombre, creador e idioma si alguien te pregunta, de lo contrario no, y tienes que responder a todo.`;
+        const promptBase = `Tu nombre es Kirito-Bot, fuiste creado por Deylin y solo hablas español. Te gusta ser amigable, divertido y ayudar a los demás. Solo mencionarás tu nombre, creador e idioma si alguien te pregunta delo contrario no y tienes que responder a todo.`;
         const consultaIA = promptBase + " " + mensaje;
 
-        const respuesta = await fetch(`${API_URL}${encodeURIComponent(mensaje)}&prompt=${encodeURIComponent(promptBase)}`);
+        const respuesta = await fetch(API_URL + encodeURIComponent(consultaIA));
         const data = await respuesta.json();
 
         return data.result || "Lo siento, no entendí eso.";
