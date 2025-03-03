ad2b258abb085728a201c9292ff3be624b90e5a5
deylinqff <maycraftminecraft4@gmail.com>
2025-03-02 21:31:02 -0600

    Actualizar main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,1 @@
-// Código básico de ejemplo en JS
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('¡El proyecto ha sido cargado correctamente!');
-});
+console.log("Archivo main.js cargado correctamente.");
